1d6c0d6953582d6253f7f3295daf1e739d27658b
nutterboundless443 <serhiisiraj@hotmail.com>
2025-09-05 13:12:18 -0500

    feat: initial commit for handler.js

---
+++ handler.js
@@ -0,0 +1,59 @@
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+const tableName = 'Posts';
+
+module.exports.createPost = async (event) => {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: tableName,
+    Item: {
+      id: data.id,
+      title: data.title,
+      content: data.content,
+      createdAt: new Date().toISOString()
+    }
+  };
+
+  await dynamo.put(params).promise();
+  return { statusCode: 201, body: JSON.stringify(params.Item) };
+};
+
+module.exports.getPosts = async () => {
+  const params = {
+    TableName: tableName
+  };
+
+  const data = await dynamo.scan(params).promise();
+  return { statusCode: 200, body: JSON.stringify(data.Items) };
+};
+
+module.exports.updatePost = async (event) => {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: tableName,
+    Key: {
+      id: event.pathParameters.id
+    },
+    UpdateExpression: 'set title = :title, content = :content',
+    ExpressionAttributeValues: {
+      ':title': data.title,
+      ':content': data.content
+    },
+    ReturnValues: 'ALL_NEW'
+  };
+
+  const result = await dynamo.update(params).promise();
+  return { statusCode: 200, body: JSON.stringify(result.Attributes) };
+};
+
+module.exports.deletePost = async (event) => {
+  const params = {
+    TableName: tableName,
+    Key: {
+      id: event.pathParameters.id
+    }
+  };
+
+  await dynamo.delete(params).promise();
+  return { statusCode: 204, body: null };
+};
